fd4cba74a72d5d588ffd589cd963c30e51eb8b27
Calvin <calvin@openmailbox.org>
2016-04-20 12:43:52 -0300

    remove tabs

--- Weather/MainForm.Designer.cs
+++ Weather/MainForm.Designer.cs
@@ -29,9 +29,6 @@
         private void InitializeComponent()
         {
             this.refreshButton = new System.Windows.Forms.Button();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.forecastPage = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
             this.forecastBox = new System.Windows.Forms.ListView();
             this.periodCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.tempCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
@@ -41,8 +38,6 @@
             this.conditionCol = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.creditsLink = new System.Windows.Forms.LinkLabel();
             this.statusLabel = new System.Windows.Forms.Label();
-            this.tabControl1.SuspendLayout();
-            this.forecastPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // refreshButton
@@ -56,40 +51,6 @@
             this.refreshButton.Text = "&Refresh";
             this.refreshButton.UseVisualStyleBackColor = true;
             // 
-            // tabControl1
-            // 
-            this.tabControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.tabControl1.Controls.Add(this.forecastPage);
-            this.tabControl1.Controls.Add(this.tabPage2);
-            this.tabControl1.Location = new System.Drawing.Point(12, 41);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(525, 278);
-            this.tabControl1.TabIndex = 1;
-            // 
-            // forecastPage
-            // 
-            this.forecastPage.Controls.Add(this.forecastBox);
-            this.forecastPage.Location = new System.Drawing.Point(4, 22);
-            this.forecastPage.Name = "forecastPage";
-            this.forecastPage.Padding = new System.Windows.Forms.Padding(3);
-            this.forecastPage.Size = new System.Drawing.Size(517, 252);
-            this.forecastPage.TabIndex = 0;
-            this.forecastPage.Text = "Forecast";
-            this.forecastPage.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 74);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
             // forecastBox
             // 
             this.forecastBox.AllowColumnReorder = true;
@@ -100,10 +61,9 @@
             this.precipCol,
             this.windCol,
             this.pressureCol});
-            this.forecastBox.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.forecastBox.Location = new System.Drawing.Point(3, 3);
+            this.forecastBox.Location = new System.Drawing.Point(12, 41);
             this.forecastBox.Name = "forecastBox";
-            this.forecastBox.Size = new System.Drawing.Size(511, 246);
+            this.forecastBox.Size = new System.Drawing.Size(525, 278);
             this.forecastBox.TabIndex = 0;
             this.forecastBox.UseCompatibleStateImageBehavior = false;
             this.forecastBox.View = System.Windows.Forms.View.Details;
@@ -166,14 +126,12 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(549, 350);
+            this.Controls.Add(this.forecastBox);
             this.Controls.Add(this.statusLabel);
             this.Controls.Add(this.creditsLink);
-            this.Controls.Add(this.tabControl1);
             this.Controls.Add(this.refreshButton);
             this.Name = "MainForm";
             this.Text = "Form1";
-            this.tabControl1.ResumeLayout(false);
-            this.forecastPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -181,9 +139,6 @@
         #endregion
 
         private System.Windows.Forms.Button refreshButton;
-        private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage forecastPage;
-        private System.Windows.Forms.TabPage tabPage2;
         private System.Windows.Forms.ListView forecastBox;
         private System.Windows.Forms.ColumnHeader periodCol;
         private System.Windows.Forms.ColumnHeader tempCol;
